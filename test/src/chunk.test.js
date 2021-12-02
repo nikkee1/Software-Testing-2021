0cf6a9fac62a3f665b6a89fb1b6df28081349bfe
Eemil Laine <eemil.laine@tuni.fi>
2021-12-02 16:03:32 +0200

    more tests done

--- test/src/chunk.test.js
+++ test/src/chunk.test.js
@@ -1,54 +1,66 @@
 import chai from 'chai';
 const expect = chai.expect;
+import chunk from "../../src/chunk.js";
 
-import capitalize from "../../src/capitalize.js";
-
-describe('capitalize.js', () => {
+describe('chunk.js', () => {
 
     describe('testing with undefined params', () => {
 
         it('should return Undefined', () => {
-            expect(capitalize()).to.eql("Undefined");
+            expect(chunk()).to.eql([]);
         });
 
         it('should return Undefined', () => {
-            expect(capitalize(undefined)).to.eql("Undefined");
+            expect(chunk(undefined)).to.eql([]);
+        });
+
+        it('should return Undefined', () => {
+            expect(chunk(undefined, undefined)).to.eql([]);
+        });
+
+        it('should return Undefined', () => {
+            expect(chunk(undefined, 3)).to.eql([]);
         });
 
     });
 
     describe('testing with unexpected params', () => {
 
-        it('should return \'\'\'\'', () => {
-            expect(capitalize("")).to.eql("");
+        it('should not return undefined', () => {
+            expect(chunk(['´', '´´´', '\'^\'', 4, 6, 3], 5)).to.not.be.undefined;
         });
 
-        it('should return \'3\', \'4\'', () => {
-            expect(capitalize("\'3\', \'4\'")).to.eql("'3', '4'");
+        it('should return [ [ 444 ], , , , ,  ]', () => {
+            expect(chunk([3,4,'f','4',444],1)).to.eql([ [ 444 ], , , , ,  ]);
         });
 
-        it('should return 123', () => {
-            expect(capitalize("123")).to.eql("123");
+        it('should return [ [ 1, \'^```\', undefined ] ]', () => {
+            expect(chunk([1,'^```',undefined], 3)).to.eql([ [ 1, '^```', undefined ] ]);
         });
 
         it('should return A[0].b.c', () => {
-            expect(capitalize('a[0].b.c')).to.eql("A[0].b.c");
+            expect(chunk([']a',[0],'.b.c'], 3)).to.eql([ [ ']a', [ 0 ], '.b.c' ] ]);
+        });
+
+        it('should return empty', () => {
+            expect(chunk(['a', 'b', 'c', 'd'], -30)).to.eql([]);
         });
 
     });
 
     describe('testing with right params', () => {
 
-        it('should return "Fred', () => {
-            expect(capitalize('FRED')).to.eql("Fred");
+        it('should return right result', () => {
+            console.log(chunk(['a', 'b', 'c', 'd'], 2));
+            expect(chunk(['a', 'b', 'c', 'd'], 2)[0]).to.eql([ 'c', 'd' ]);
         });
 
-        it('should return ´fred´', () => {
-            expect(capitalize('´FRED´')).to.eql("´fred´");
+        it('should return right result', () => {
+            expect(chunk(['a', 'b', 'c', 'd'], 3)[0]).to.eql([ 'd', undefined, undefined ]);
         });
 
-        it('should return -fred-', () => {
-            expect(capitalize('-FRED-')).to.eql("-fred-");
+        it('should return right result', () => {
+            expect(chunk(['a', 'b', 'c', 'd', 'e'], 5)[0]).to.eql([ 'a', 'b', 'c', 'd', 'e' ]);
         });
 
     });
