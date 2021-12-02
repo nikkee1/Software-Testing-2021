251c07e2d80ef20a5014cf4a5b3a24cc3a5fd53d
Eemil Laine <eemil.laine@tuni.fi>
2021-12-02 15:19:54 +0200

    tests added

--- test/src/castArray.test.js
+++ test/src/castArray.test.js
@@ -1,2 +1,56 @@
 import chai from 'chai';
 const expect = chai.expect;
+import castArray from "../../src/castArray.js";
+
+describe('castArray.js', () => {
+
+    describe('testing with undefined params', () => {
+
+        it('should return Undefined', () => {
+            expect(castArray()).to.eql([undefined]);
+        });
+
+        it('should return Undefined', () => {
+            expect(castArray(undefined)).to.eql([undefined]);
+        });
+
+    });
+
+    describe('testing with unexpected params', () => {
+
+        it('should return false', () => {
+            expect(castArray([{name: 'Test Person', address: 'Test Avenue 3', zip: 322223}])).to.
+            not.be.true;
+        });
+
+        it('should return false', () => {
+            expect(castArray("\'3\', \'4\'")).to.not.be.true;
+        });
+
+        it('should return false', () => {
+            expect(castArray("123")).to.not.be.true;
+        });
+
+        it('should return false', () => {
+            expect(castArray('a',[0],'.b.c')).to.not.be.true;
+        });
+
+    });
+
+    describe('testing with right params', () => {
+
+        it('should return [1, 2, 3]', () => {
+            expect(castArray([1, 2, 3])).to.eql([1, 2, 3]);
+        });
+
+        it('should return []', () => {
+            expect(castArray([])).to.eql([]);
+        });
+
+        it('should return [1,\'2\', 3]', () => {
+            expect(castArray([1, "2", 3])).to.eql([1, "2", 3]);
+        });
+
+    });
+
+});
--- test/src/ceil.test.js
+++ test/src/ceil.test.js
@@ -1,2 +1,55 @@
 import chai from 'chai';
 const expect = chai.expect;
+import ceil from "../../src/ceil.js";
+
+describe('ceil.js', () => {
+
+    describe('testing with undefined params', () => {
+
+        it('ceil() -> should return NaN', () => {
+            expect(ceil()).to.eql(NaN);
+        });
+
+        it('ceil(undefined) -> should return NaN', () => {
+            expect(ceil(undefined)).to.eql(NaN);
+        });
+
+    });
+
+    describe('testing with unexpected params', () => {
+
+        it('ceil("\'\'\'\'") -> should return NaN', () => {
+            expect(ceil("\'\'\'\'")).to.eql(NaN);
+        });
+
+        it('ceil("\'3\', \'4\'") -> should return NaN', () => {
+            expect(ceil("\'3\', \'4\'")).to.eql(NaN);
+        });
+
+        it('ceil("123") -> should return 123', () => {
+            expect(ceil("123")).to.eql(123);
+        });
+
+        it('ceil(\'a[0].b.c\') -> should return NaN', () => {
+            expect(ceil('a[0].b.c')).to.eql(NaN);
+        });
+
+    });
+
+    describe('testing with right params', () => {
+
+        it('ceil(4.006) -> should return 5', () => {
+            expect(ceil(4.006)).to.eql(5);
+        });
+
+        it('ceil(6.004, 2) -> should return 6.01', () => {
+            expect(ceil(6.004, 2)).to.eql(6.01);
+        });
+
+        it('ceil(6040, -2) -> should return 6100', () => {
+            expect(ceil(6040, -2)).to.eql(6100);
+        });
+
+    });
+
+});
