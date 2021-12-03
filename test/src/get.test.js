71d1a85442d7a784a7bbff7107acf5ee43afa584
Niklas Savijoki <niklas.savijoki@tuni.fi>
2021-12-03 12:58:16 +0200

    get.test added

--- test/src/get.test.js
+++ test/src/get.test.js
@@ -1,2 +1,40 @@
 import chai from 'chai';
 const expect = chai.expect;
+import get from "../../src/get.js";
+
+const television = {
+    price: "1000",
+    producer: "Sony",
+    category: "entertainment"
+}
+
+const candy = {
+    price: "0.5",
+    producer: "Fazer",
+    category: "food",
+    subcategory: ""
+}
+
+describe('get.js', () => {
+
+    describe('Testing with unexpected input', () => {
+
+        it('should return default value when undefined input', () => {
+            expect(get(television, 'subcategory', 'Not set')).to.eql('Not set');
+        });
+    });
+
+    describe('Testing with expected/common input', () => {
+
+        it('should return correct price when price is below 1', () => {
+            expect(get(candy, 'price')).to.eql('0.5');
+        });
+        it('should return correct price when price is above 1', () => {
+            expect(get(television, 'price')).to.eql('1000');
+        });
+        it('should return empty string', () => {
+            expect(get(candy, 'subcategory')).to.eql('');
+        });
+    });
+
+});
